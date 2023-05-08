e69b093eda193bb98f99f41fba696df532d18c67
ddenxie <ddenxie2026@gmail.com>
2023-05-08 17:23:51 +0800

    05/08/2023

--- database/migrations/2023_05_08_080456_create_user_table.php
+++ database/migrations/2023_05_08_080456_create_user_table.php
@@ -4,8 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
-
 return new class extends Migration
 {
     /**
@@ -13,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string(column:'Users');
-            $table->string(column:'email');
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('user');
     }
 };
