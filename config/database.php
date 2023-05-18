95c8fad57cd50db6fc37a977c293730d0f9bd606
ddenxie <ddenxie2026@gmail.com>
2023-05-18 17:52:12 +0800

    project_backend5/18/23

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', default:'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -46,12 +46,12 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => env('DB_HOST', default:'127.0.0.1'),
+            'port' => env('DB_PORT', default:'3306'),
+            'database' => env('DB_DATABASE', default:''),
+            'username' => env('DB_USERNAME', default:''),
+            'password' => env('DB_PASSWORD', default:''),
+            'unix_socket' => env('DB_SOCKET', default:''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
